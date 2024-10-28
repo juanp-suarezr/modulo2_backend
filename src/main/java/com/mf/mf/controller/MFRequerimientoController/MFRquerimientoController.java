2f3eb0485998ac451b1d63a7972bc8a61ee6558a
Cristian Algecira <cristian.algecira.99@gamil.com>
2024-10-28 13:09:12 -0500

    Cambio 4

--- src/main/java/com/mf/mf/controller/MFRequerimientoController/MFRquerimientoController.java
+++ src/main/java/com/mf/mf/controller/MFRequerimientoController/MFRquerimientoController.java
@@ -1,10 +1,12 @@
 package com.mf.mf.controller.MFRequerimientoController;
 
 import com.mf.mf.dto.MFRequerimientoDTO;
+import com.mf.mf.payload.ApiResponse;
 import com.mf.mf.services.MFRequerimientoServices.MFRequerimientoServices;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -20,7 +22,6 @@ public class MFRquerimientoController {
         MFRequerimientoDTO saved_item = requerimientoServices.save(body);
         ApiResponse<MFRequerimientoDTO> response = new ApiResponse<>("Registro almacenado exitosamente", saved_item);
         return ResponseEntity.ok(response);
-
     }
 
 }
