8f8c929e977f36c078310b2f5cab59890d6cbe73
Cristian Algecira <cristian.algecira.99@gamil.com>
2024-12-11 16:25:04 -0500

    Cambio 13

---
+++ src/main/java/com/mf/mf/model/excel/MFDictamenRevisorFiscal.java
@@ -0,0 +1,23 @@
+package com.mf.mf.model.excel;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(schema = "financiero", name = "\"MFDictamenRevisorFiscal\"")
+public class MFDictamenRevisorFiscal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Boolean estado;
+    private String dictamen;
+    private String opinionDictamen;
+    private String contenidoSalvedadDictamen;
+    private String parrafoEnfasisDictamen;
+    private String hipotesisNegocioMarcha;
+    private String validacionHNM;
+    private Long nit;
+    private Integer annio;
+    
+}
---
+++ src/main/java/com/mf/mf/model/excel/MFEstadoCambioPatrimonio.java
@@ -0,0 +1,35 @@
+package com.mf.mf.model.excel;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(schema = "financiero", name = "\"MFEstadoCambioPatrimonio\"")
+public class MFEstadoCambioPatrimonio {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Boolean estado;
+    private Integer saldoInicialPeriodoActual;
+    private Integer disminucionIncrementoDistribucionesPropietarios;
+    private Integer incrementoDisminucionTransferenciasOtrosCambios;
+    private Integer incrementoDisminucionTransaccionesAccionesCartera;
+    private Integer incrementoDisminucionCambiosParticipacionSubsidiarias;
+    private Integer totalCambiosPatrimonio;
+    private Integer saldoFinalPeriodoActual;
+    private Integer incrementoDisminucionPoliticasContables;
+    private Integer incrementoDisminucionCorreccionesErrores;
+    private Integer saldoInicialReexpresado;
+    private Integer cambiosEnPatrimonio;
+    private Integer resultadoIntegral;
+    private Integer gananciaPerdida;
+    private Integer otroResultadoIntegral;
+    private Integer resultadoIntegralFinal;
+    private Integer emisionPatrimonio;
+    private Integer dividendos;
+    private Integer incrementoDisminucionAportacionesPropietarios;
+    private Integer annio;
+    private Integer nit;
+    
+}
